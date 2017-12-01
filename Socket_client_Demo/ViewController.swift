852662060c0d5e7768b8339b3200ffb31c857ab1
sexyfather88 <sexyfather88@yahoo.com.tw>
2017-12-01 21:30:42 +0800

    Initial Commit

---
+++ Socket_client_Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Socket_client_Demo
+//
+//  Created by Wilson on 2017/12/1.
+//  Copyright © 2017年 Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
