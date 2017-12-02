46afdcf700ad60c79e618950eaf1a542bbcf794f
sexyfather88 <sexyfather88@yahoo.com.tw>
2017-12-02 12:50:30 +0800

    client connect to locahost do retrieving and send message

--- Socket_client_Demo/AppDelegate.swift
+++ Socket_client_Demo/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        self.window=UIWindow.init(frame: UIScreen.main.bounds)
+        
+        let page:ViewController=ViewController(nibName:"ViewController",bundle:nil)
+
+        self.window?.rootViewController=page
+        self.window?.makeKeyAndVisible()
         return true
     }
 
--- Socket_client_Demo/ViewController.swift
+++ Socket_client_Demo/ViewController.swift
@@ -10,9 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var retrieveLabel: UILabel!
+    @IBOutlet weak var sendTextField: UITextField!
+
+    var iStream:InputStream?=nil
+    var oStream:OutputStream?=nil
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        Stream.getStreamsToHost(withName: "localhost", port:5000, inputStream: &iStream, outputStream: &oStream)
+        
+        iStream?.open()
+        oStream?.open()
+        
+        DispatchQueue.global().async {
+            
+                self.retrieveData(available: { (string) in
+                    DispatchQueue.main.async {
+                        self.retrieveLabel.text=string
+                    }
+                })
+        }
+    }
+    
+
+    func retrieveData(available:(_ string:String?) -> Void) {
+    
+        var buff=Array(repeating: UInt8(0),count:1024)
+        
+        while true {
+            if let n=iStream?.read(&buff, maxLength: 1024)
+            {
+                if(n<0)
+                {
+                    return
+                }
+                
+                let data=Data(bytes:buff,count:n)
+                let string=String(data:data,encoding:.utf8)
+                available(string)
+            }
+        }
+    
+    }
+    func send(_ string:String){
+        
+        var buff=Array(repeating: UInt8(0),count:1024)
+        let  data=string.data(using: .utf8)
+        
+        data?.copyBytes(to: &buff,count:(data?.count)!)
+        oStream?.write(buff, maxLength: (data?.count)!)
+    }
+    
+    @IBAction func onSendClicked()
+    {
+        if let string=self.sendTextField.text
+        {
+            send(string)
+        }
+    }
+    
+    @IBAction func handleDismissKeyboard()
+    {
+        self.sendTextField.resignFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
